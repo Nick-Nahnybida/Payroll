2e80c3efa1f068de2740300c7398ff9fb8028d33
Nick-Nahnybida <nicknahnybida@gmail.com>
2016-09-30 10:19:24 -0500

    Add files via upload

---
+++ Payroll/Payroll/Form1.Designer.vb
@@ -0,0 +1,293 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtSSN = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtPayRate = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtHoursWorked = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblGrossPay = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblFedTax = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblStateTax = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.lblNetPay = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(271, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Payroll"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(46, 74)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Name:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(135, 74)
+        Me.txtName.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(148, 22)
+        Me.txtName.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(322, 59)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(116, 32)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Social Security " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Number:"
+        '
+        'txtSSN
+        '
+        Me.txtSSN.Location = New System.Drawing.Point(446, 74)
+        Me.txtSSN.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtSSN.Name = "txtSSN"
+        Me.txtSSN.Size = New System.Drawing.Size(148, 22)
+        Me.txtSSN.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(350, 142)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(88, 32)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Hourly Pay " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Rate:"
+        '
+        'txtPayRate
+        '
+        Me.txtPayRate.Location = New System.Drawing.Point(446, 156)
+        Me.txtPayRate.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtPayRate.Name = "txtPayRate"
+        Me.txtPayRate.Size = New System.Drawing.Size(148, 22)
+        Me.txtPayRate.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(27, 142)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(66, 32)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Hours " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Worked:"
+        '
+        'txtHoursWorked
+        '
+        Me.txtHoursWorked.Location = New System.Drawing.Point(135, 156)
+        Me.txtHoursWorked.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtHoursWorked.Name = "txtHoursWorked"
+        Me.txtHoursWorked.Size = New System.Drawing.Size(148, 22)
+        Me.txtHoursWorked.TabIndex = 8
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(246, 454)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(112, 28)
+        Me.btnClear.TabIndex = 9
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(440, 454)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(112, 28)
+        Me.btnExit.TabIndex = 10
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(18, 313)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(84, 16)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Gross Pay:"
+        '
+        'lblGrossPay
+        '
+        Me.lblGrossPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblGrossPay.Location = New System.Drawing.Point(133, 313)
+        Me.lblGrossPay.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblGrossPay.Name = "lblGrossPay"
+        Me.lblGrossPay.Size = New System.Drawing.Size(150, 28)
+        Me.lblGrossPay.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(311, 314)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(96, 16)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Federal Tax:"
+        '
+        'lblFedTax
+        '
+        Me.lblFedTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFedTax.Location = New System.Drawing.Point(446, 313)
+        Me.lblFedTax.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblFedTax.Name = "lblFedTax"
+        Me.lblFedTax.Size = New System.Drawing.Size(150, 28)
+        Me.lblFedTax.TabIndex = 14
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(24, 379)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(78, 16)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "State Tax:"
+        '
+        'lblStateTax
+        '
+        Me.lblStateTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblStateTax.Location = New System.Drawing.Point(133, 379)
+        Me.lblStateTax.Name = "lblStateTax"
+        Me.lblStateTax.Size = New System.Drawing.Size(150, 28)
+        Me.lblStateTax.TabIndex = 16
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(314, 379)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(67, 16)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Net Pay:"
+        '
+        'lblNetPay
+        '
+        Me.lblNetPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblNetPay.Location = New System.Drawing.Point(446, 367)
+        Me.lblNetPay.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblNetPay.Name = "lblNetPay"
+        Me.lblNetPay.Size = New System.Drawing.Size(150, 28)
+        Me.lblNetPay.TabIndex = 18
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(49, 454)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(112, 28)
+        Me.btnCalculate.TabIndex = 19
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(621, 528)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblNetPay)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.lblStateTax)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.lblFedTax)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.lblGrossPay)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.txtHoursWorked)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtPayRate)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtSSN)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtSSN As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtPayRate As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtHoursWorked As System.Windows.Forms.TextBox
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents lblGrossPay As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents lblFedTax As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents lblStateTax As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents lblNetPay As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+
+End Class
---
+++ Payroll/Payroll/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtName.Clear()
+        txtSSN.Clear()
+        txtPayRate.Clear()
+        txtHoursWorked.Clear()
+        lblGrossPay.Text = ""
+        lblFedTax.Text = ""
+        lblStateTax.Text = ""
+        lblNetPay.Text = ""
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim grossPay, fedTax, stateTax, netPay As Double
+
+        grossPay = CDbl(txtPayRate.Text) * CDbl(txtHoursWorked.Text)
+        'Display the result.
+        lblGrossPay.Text = CStr(grossPay)
+
+        fedTax = CDbl(grossPay) * 0.15
+        lblFedTax.Text = CStr(fedTax)
+
+        stateTax = CDbl(grossPay) * 0.05
+        lblStateTax.Text = CStr(stateTax)
+
+        netPay = CDbl(grossPay) - (fedTax + stateTax)
+        lblNetPay.Text = CStr(netPay)
+    End Sub
+End Class
